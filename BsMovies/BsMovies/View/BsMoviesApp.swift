b1e77e863400086ee9ed365474b07839d5fa8096
Mahmudullah <mahmudullahmdev@gmail.com>
2022-08-12 11:00:11 +0600

    - init project - project skeleton added. - add network util classes [pending]

---
+++ BsMovies/BsMovies/View/BsMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BsMoviesApp.swift
+//  BsMovies
+//
+//  Created by Mahmudullah on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BsMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ BsMovies/BsMovies/View/Home/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BsMovies
+//
+//  Created by Mahmudullah on 12/8/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
